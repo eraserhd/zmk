38bbfab8c7f684cbcbd0af7589660adf20196de0
Jason Felice <jason.m.felice@gmail.com>
2022-05-31 10:25:59 -0400

    Use nix-zmk

--- flake.nix
+++ flake.nix
@@ -1,8 +1,10 @@
 {
   inputs = {
     flake-utils.url = "github:numtide/flake-utils";
+    nix-zmk.url = "github:eraserhd/nix-zmk";
+    nix-zmk.inputs.nixpkgs.follows = "nixpkgs";
   };
-  outputs = { self, flake-utils, nixpkgs }:
+  outputs = { self, flake-utils, nixpkgs, nix-zmk }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
