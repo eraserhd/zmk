aadd21248343f5699afe38a7d7ff55ec2efaee96
Jason Felice <jason.m.felice@gmail.com>
2022-06-02 04:12:50 -0400

    Use from github

--- flake.nix
+++ flake.nix
@@ -1,16 +1,14 @@
 {
   inputs = {
     flake-utils.url = "github:numtide/flake-utils";
-    nix-zmk.url = "/home/jfelice/src/nix-zmk";
+    nix-zmk.url = "github:eraserhd/nix-zmk";
     nix-zmk.inputs.nixpkgs.follows = "nixpkgs";
   };
   outputs = { self, flake-utils, nixpkgs, nix-zmk }:
     flake-utils.lib.eachDefaultSystem (system: {
-      devShells.default = nix-zmk.lib.devShell {
-        inherit nixpkgs system;
-      };
+      devShells.default = nix-zmk.lib.devShell { inherit system; };
       packages.default = nix-zmk.lib.firmwarePackage {
-        inherit nixpkgs system;
+        inherit system;
         name = "corne-firmware";
         board = "nice_nano";
         shields = [ "corne_left" "corne_right" ];
